414dd36a949fde8edff92a71a8a918946c47cad7
Craig Sloane <craigrussellsloane@gmail.com>
2020-08-10 22:56:05 +0100

    fix typo

--- backend/src/lamdba/http/getPosts.ts
+++ backend/src/lamdba/http/getPosts.ts
@@ -1,28 +1,22 @@
 import "source-map-support/register";
 import { getUserId } from "../utils";
-import { getAllTodos } from "../../businessLogic/todos";
+import { getAllPosts } from "../../businessLogic/posts";
 import {
   APIGatewayProxyEvent,
   APIGatewayProxyResult,
   APIGatewayProxyHandler,
 } from "aws-lambda";
 import { createLogger } from "../../utils/logger";
-import { S3Helper } from "../../helpers/s3Helper";
 
 const logger = createLogger("getTodos");
-const s3Helper = new S3Helper();
 
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   const user = getUserId(event);
-  const items = await getAllTodos(user);
+  const items = await getAllPosts(user);
 
   logger.info(`create group for user ${user}`);
-
-  for (const item of items) {
-    item.attachmentUrl = await s3Helper.getTodoAttachmentUrl(item.todoId);
-  }
 
   return {
     statusCode: 200,
