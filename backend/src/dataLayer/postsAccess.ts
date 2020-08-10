4be9365653ab93e7804af20343ca4b5397ba8a4a
Craig Sloane <craigrussellsloane@gmail.com>
2020-08-10 23:15:52 +0100

    resolve linting errors

--- backend/src/dataLayer/postsAccess.ts
+++ backend/src/dataLayer/postsAccess.ts
@@ -1,19 +1,13 @@
 import { UserPost } from "../models/UserPost";
 import * as AWS from "aws-sdk";
-import * as AWSXRay from "aws-xray-sdk";
-import { CreatePostRequest } from "../requests/CreatePostRequest";
-import { UpdatePostRequest } from "../requests/UpdatePostRequest";
-import uuid from "uuid/v4";
 import { DocumentClient } from "aws-sdk/clients/dynamodb";
 import { createLogger } from "../utils/logger";
-
-const XAWS = AWSXRay.captureAWS(AWS);
 
 const logger = createLogger("postsAccess");
 
 export class PostsAccess {
   constructor(
-    private readonly docClient: DocumentClient = createDynamoDBClient(),
+    private readonly docClient: DocumentClient = new AWS.DynamoDB.DocumentClient(),
     private readonly postsTable = process.env.POSTS_TABLE,
     private readonly indexName = process.env.INDEX_NAME,
     private readonly userIndex = process.env.USER_ID_INDEX
@@ -38,16 +32,4 @@ export class PostsAccess {
 
     return result.Items as UserPost[];
   }
-}
-
-function createDynamoDBClient() {
-  if (process.env.IS_OFFLINE) {
-    console.log("Creating a local DynamoDB instance");
-    return new XAWS.DynamoDB.DocumentClient({
-      region: "localhost",
-      endpoint: "http://localhost:8000",
-    });
-  }
-
-  return new XAWS.DynamoDB.DocumentClient();
 }
